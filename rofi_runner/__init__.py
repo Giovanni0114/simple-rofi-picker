8bd3c841a46ee934104f071e2f9dd41ed666d754
Jan Juroszek <janko.juroszek@gmail.com>
2024-07-20 12:16:51 +0200

    fix: label, not value was passed to command

--- rofi_runner/__init__.py
+++ rofi_runner/__init__.py
@@ -1,6 +1,5 @@
 import subprocess
-import config
-
+from config import Config, Option
 
 class Result:
     success: bool
@@ -21,15 +20,16 @@ class RofiRunner:
     last_result = None | Result
 
     def get_rofi_command(self) -> str:
-        conf = config.Config()
+        conf = Config()
         return f'echo -e "{"\n".join(conf.get_options_labels())}" | rofi -dmenu {'-i' if conf.case_sensitive else ''} -p "{conf.name}"'
 
-    def select_option(self) -> str:
+    def select_option(self) -> Option:
         rofi_command = self.get_rofi_command()
-        return subprocess.run(rofi_command, shell=True, capture_output=True, text=True).stdout.strip()
+        option_label = subprocess.run(rofi_command, shell=True, capture_output=True, text=True).stdout.strip()
+        return Config().get_option_by_label(option_label)
 
     def run(self) -> Result:
-        conf = config.Config()
+        conf = Config()
         if not conf.is_valid():
             res = Result(False, "Invalid config")
             self.last_result = res
@@ -37,7 +37,8 @@ class RofiRunner:
 
         option = self.select_option()
 
-        command = conf.get_base_command().replace("%1", option)
+        command = conf.get_base_command().replace("%1", option.value)
 
         if command:
             subprocess.run(command, shell=True)
+
